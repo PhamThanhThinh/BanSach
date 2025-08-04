b1b03569f8f536c7b09001408d1794c2791f89ef
thinh <thinhqn999@gmail.com>
2025-08-04 21:04:29 +0700

    add class cart detail

--- BanSach/Models/CartDetail.cs
+++ BanSach/Models/CartDetail.cs
@@ -1,10 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BanSach.Models
 {
   [Table("CartDetail")]
   public class CartDetail
   {
+    public int Id { get; set; }
+
+    [Required]
+    public int ShoppingCartId { get; set; }
+    public ShoppingCart ShoppingCart { get; set; }
+
+    [Required]
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+
+    // số lượng của một cuốn
+    [Required]
+    public int Quantity { get; set; }
 
   }
 }
