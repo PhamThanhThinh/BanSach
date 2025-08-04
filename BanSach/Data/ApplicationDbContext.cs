5e9c6ca5fc06817d581e47b05a9de726ba47b55a
thinh <thinhqn999@gmail.com>
2025-08-04 22:20:00 +0700

    update-database

--- BanSach/Data/ApplicationDbContext.cs
+++ BanSach/Data/ApplicationDbContext.cs
@@ -12,7 +12,12 @@ namespace BanSach.Data
     }
 
     public DbSet<Book> Books { get; set; }
+    public DbSet<CartDetail> CartDetails { get; set; }
     public DbSet<Genre> Genres { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<OrderStatus> OrderStatuses { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
   }
 }
--- BanSach/Data/Migrations/20250804151437_them cac truong va them cac bang vao database.Designer.cs
+++ BanSach/Data/Migrations/20250804151437_them cac truong va them cac bang vao database.Designer.cs
@@ -4,6 +4,7 @@ using BanSach.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BanSach.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250804151437_them cac truong va them cac bang vao database")]
+    partial class themcactruongvathemcacbangvaodatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
