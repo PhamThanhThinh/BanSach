ef98062e6ccad4c410afa72699ce879c61c5c633
thinh <thinhqn999@gmail.com>
2025-08-04 21:18:48 +0700

    docs(note): add git commit convention & thêm trường img để hiện thị hình ảnh

--- BanSach/Models/Book.cs
+++ BanSach/Models/Book.cs
@@ -7,9 +7,12 @@ namespace BanSach.Models
   public class Book
   {
     public int Id { get; set; }
+
     [Required]
     [MaxLength(50)]
-    public string BookName { get; set; }
+    public string? BookName { get; set; }
+
+    public string? Image { get; set; }
 
     [Required]
     public int GenreId { get; set; }
