d8b4e7f74bfbc158a85736b7cafb8afff60108fb
thinh <thinhqn999@gmail.com>
2025-08-26 01:00:40 +0700

    phần 10

---
+++ BanSach/Repo/HomeRepository.cs
@@ -0,0 +1,22 @@
+﻿using BanSach.Data;
+
+namespace BanSach.Repo
+{
+  public class HomeRepository
+  {
+    private readonly ApplicationDbContext _db;
+
+    public HomeRepository(ApplicationDbContext db)
+    {
+      _db = db;
+    }
+
+    // quy ước giá trị mặc định
+    // term = ""
+    // categoryId = 0
+    public void DisplayBooks(string term = "", int categoryId = 0)
+    {
+
+    }
+  }
+}
